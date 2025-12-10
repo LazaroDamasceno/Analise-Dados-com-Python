16096ef7beec73f502b6bdaaad3282343753237c
LazaroDamasceno <lazarolawrence2@gmail.com>
2025-12-10 17:21:41 -0300

    Alterou iqr.py

--- iqr.py
+++ iqr.py
@@ -1,25 +1,18 @@
 import polars as pl
-import pandas as pd
 
-def find_outliers_polars(df: pl.DataFrame, column: str | int | float) -> pl.DataFrame:
-    Q1 = df.select(pl.col(column).quantile(0.25).alias("Q1")).item()
-    Q3 = df.select(pl.col(column).quantile(0.75).alias("Q3")).item()
-    IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-    df = df.with_columns(
-        is_outlier=(
-            (pl.col(column) < lower_bound) | (pl.col(column) > upper_bound)
-        )
-    )
-    return df
-
-def find_outliers_pandas(df: pd.DataFrame, column: str | int | float) -> pd.DataFrame:
+def find_outliers_pandas(df, column):
     Q1 = df[column].quantile(0.25)
     Q3 = df[column].quantile(0.75)
     IQR = Q3 - Q1
     lower_bound = Q1 - 1.5 * IQR
     upper_bound = Q3 + 1.5 * IQR
-    is_outlier = (df[column] < lower_bound) | (df[column] > upper_bound)
-    df['is_outlier'] = is_outlier
-    return df
+    return (df[column] < lower_bound) | (df[column] > upper_bound)
+
+def find_outliers_polars(df, column):
+    Q1 = df.select(pl.col(column).quantile(0.25)).item()
+    Q3 = df.select(pl.col(column).quantile(0.75)).item()
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    outliers_expression = (pl.col(column) < lower_bound) | (pl.col(column) > upper_bound)
+    return df.select(outliers_expression).to_series().any()

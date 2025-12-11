e51a28767c1849132800a98d323adb3c3100c740
LazaroDamasceno <lazarolawrence2@gmail.com>
2025-12-10 23:59:56 -0300

    Criou a pasta kaggle/city_lifestyle_segmentation-_dataset

--- skewness.py
+++ skewness.py
@@ -1,9 +1,11 @@
 import polars as pl
 
-def is_right_skewed_polars(df, column):
+def is_right_skewed_log(df, column):
     skew = df.select(pl.col(column).skew()).item()
     has_zeroes_or_negatives = df.filter(pl.col(column) <= 0.0).height > 0
-    if (skew > 1 and has_zeroes_or_negatives): 
-        print(f'Para {column}: Use np.log1p().')
-    elif (skew > 1 and not has_zeroes_or_negatives): 
-        print(f'Para {column}: Use np.log().')
+    return skew > 1 and not has_zeroes_or_negatives
+
+def is_right_skewed_log1p(df, column):
+    skew = df.select(pl.col(column).skew()).item()
+    has_zeroes_or_negatives = df.filter(pl.col(column) <= 0.0).height > 0
+    return skew > 1 and has_zeroes_or_negatives

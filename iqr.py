561a8af5cae86fd5456c15e4a9ce004bc581c6fe
LazaroDamasceno <lazarolawrence2@gmail.com>
2025-12-12 19:51:27 -0300

    Criou o arquivo (incompleto)analise.py

--- iqr.py
+++ iqr.py
@@ -1,13 +1,5 @@
 import polars as pl
 
-def find_outliers_pandas(df, column):
-    Q1 = df[column].quantile(0.25)
-    Q3 = df[column].quantile(0.75)
-    IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-    return (df[column] < lower_bound) | (df[column] > upper_bound)
-
 def find_outliers_polars(df, column):
     Q1 = df.select(pl.col(column).quantile(0.25)).item()
     Q3 = df.select(pl.col(column).quantile(0.75)).item()
@@ -16,3 +8,12 @@ def find_outliers_polars(df, column):
     upper_bound = Q3 + 1.5 * IQR
     outliers_expression = (pl.col(column) < lower_bound) | (pl.col(column) > upper_bound)
     return df.select(outliers_expression).to_series().any()
+
+def find_outliers_pandas(df, column):
+    series = df[column]
+    Q1 = series.quantile(0.25)
+    Q3 = series.quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    return (series < lower_bound) | (series > upper_bound)
--- skewness.py
+++ skewness.py
@@ -2,10 +2,10 @@ import polars as pl
 
 def is_right_skewed_log(df, column):
     skew = df.select(pl.col(column).skew()).item()
-    has_zeroes_or_negatives = df.filter(pl.col(column) <= 0.0).height > 0
+    has_zeroes_or_negatives = df.filter(pl.col(column) <= 0).height > 0
     return skew > 1 and not has_zeroes_or_negatives
 
 def is_right_skewed_log1p(df, column):
     skew = df.select(pl.col(column).skew()).item()
-    has_zeroes_or_negatives = df.filter(pl.col(column) <= 0.0).height > 0
+    has_zeroes_or_negatives = df.filter(pl.col(column) <= 0).height > 0
     return skew > 1 and has_zeroes_or_negatives

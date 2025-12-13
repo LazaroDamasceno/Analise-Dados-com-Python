b3fb2e67f96e007d50dfa216896d027c2468714d
LazaroDamasceno <lazarolawrence2@gmail.com>
2025-12-12 23:40:42 -0300

    Renomeou analise.ipynb como clustering_city_lifestyle_segmentation.ipynb

--- pick_kmeans_dbscan.py
+++ pick_kmeans_dbscan.py
@@ -1,6 +1,8 @@
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.metrics import silhouette_score
 
+X_pca = None
+
 kmeans = KMeans(n_clusters=5, random_state=42)
 labels_kmeans = kmeans.fit_predict(X_pca)
 score_kmeans = silhouette_score(X_pca, labels_kmeans)
@@ -10,5 +12,5 @@ labels_dbscan = dbscan.fit_predict(X_pca)
 mask = labels_dbscan != -1
 score_dbscan = silhouette_score(X_pca[mask], labels_dbscan[mask])
 
-print("Silhouette KMeans:", score_kmeans)
-print("Silhouette DBSCAN:", score_dbscan)
+print("Silhouette KMeans: ", score_kmeans)
+print("Silhouette DBSCAN: ", score_dbscan)
